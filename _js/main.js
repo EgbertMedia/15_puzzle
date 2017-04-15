b43b679d42f4c5b0c445ffce045159e1e28791dc
Egbert Sluiter <ejsluiter@gmail.com>
2017-04-15 14:52:24 +0200

    Add puzzle pieces

--- _js/main.js
+++ _js/main.js
@@ -1,4 +1,27 @@
 var emptyCell = {'x': 3, 'y': 3};
+var gridSize = 4;
+
+function createTile(x, y) {
+  var tile = $('<div />', {'class':  'puzzlePiece'});
+  tile.css('left', (x*100)+"px");
+  tile.css('top', (y*100)+"px");
+  tile.css('background-position', 'left '+ (-(x*100)) + 'px top ' + (-(y*100)) + 'px');
+
+  $('div#puzzleContainer').append(tile);
+
+}
+
+function initPuzzle() {
+  var tileCount = 0;
+  for (var i = 0; i < gridSize; i++) {
+    for (var j = 0; j < gridSize; j++) {
+      if (tileCount < (gridSize*gridSize-1)) {
+        createTile(j,i);
+        tileCount++;
+      }
+    }
+  }
+}
 
 function randomize() {
 
@@ -9,5 +32,5 @@ function move() {
 }
 
 $(document).ready(function() {
-
+  initPuzzle();
 });
