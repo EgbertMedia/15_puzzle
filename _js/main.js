cb8b97cb6a9858e940e61da1dfc504176c34c71e
Egbert Sluiter <ejsluiter@gmail.com>
2017-04-15 16:35:41 +0200

    Add tile movement

--- _js/main.js
+++ _js/main.js
@@ -1,6 +1,7 @@
 var emptyCell = {'x': 3, 'y': 3};
 var tilePositions = [];
 var gridSize = 4;
+var animationDuration = 1000;
 
 function createTile(x, y) {
   var tile = $('<div />', {'class':  'puzzlePiece'});
@@ -29,34 +30,60 @@ function randomize() {
 
 }
 
+function moveTile(tileNumber, x, y) {
+  tileNumber++; // CSS :nth-child starts with 1 instead of 0
+  console.log(tileNumber);
+  $('div.puzzlePiece:nth-child('+tileNumber+')').animate({
+        'left': (x*100)+"px",
+        'top': (y*100)+"px",
+        'background-position': 'left '+ (-(x*100)) + 'px top ' + (-(y*100)) + 'px'
+        }, animationDuration, function() {
+    $('div.puzzlePiece:nth-child('+tileNumber+')').data('x', x);
+    $('div.puzzlePiece:nth-child('+tileNumber+')').data('y', y);
+  });
+}
+
 function moveHorizontal(data) {
   console.log('horizontal');
-  $('.puzzlePiece').each(function() {
-    if (data.x > emptyCell.x) {
+
+  if (data.x > emptyCell.x) {
+    $('.puzzlePiece').each(function() {
       if ($(this).data().y === emptyCell.y && $(this).data().y <= data.y) {
         console.log('left');
+        moveTile($(this).index(), $(this).data().x - 1, $(this).data().y);
       }
-    } else {
+    });
+    emptyCell.x = 3;
+  } else {
+    $('.puzzlePiece').each(function() {
       if ($(this).data().y === emptyCell.y && $(this).data().y >= data.y) {
         console.log('right');
+        moveTile($(this).index(), $(this).data().x + 1, $(this).data().y);
       }
-    }
-  });
+    });
+    emptyCell.x = 0;
+  }
 }
 
 function moveVertical(data) {
   console.log('vertical');
-  $('.puzzlePiece').each(function() {
-    if (data.y > emptyCell.y) {
+  if (data.y > emptyCell.y) {
+    $('.puzzlePiece').each(function() {
       if ($(this).data().x === emptyCell.x && $(this).data().x <= data.x) {
         console.log('up');
+        moveTile($(this).index(), $(this).data().x, $(this).data().y - 1);
       }
-    } else {
+    });
+    emptyCell.y = 3;
+  } else {
+    $('.puzzlePiece').each(function() {
       if ($(this).data().x === emptyCell.x && $(this).data().x >= data.x) {
         console.log('down');
+        moveTile($(this).index(), $(this).data().x, $(this).data().y + 1);
       }
-    }
-  });
+    });
+    emptyCell.y = 0;
+  }
 }
 
 function move(data) {
